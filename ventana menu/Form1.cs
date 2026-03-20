b419cba5ac1cecb432d3ff7732f8c417b1d91ad5
Paletadelmal <ingridrosasre2006@gmail.com>
2026-03-20 02:01:01 -0600

    pull

--- ventana menu/Form1.cs
+++ ventana menu/Form1.cs
@@ -1,3 +1,6 @@
+﻿using System;
+using System.Windows.Forms;
+
 namespace ventana_menu
 {
     public partial class Form1 : Form
@@ -50,6 +53,30 @@ namespace ventana_menu
         private void label14_Click(object sender, EventArgs e)
         {
 
+        }
+
+        // 🔘 BOTÓN LOGIN AGREGADO
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Validación básica
+            string usuario = txtUsuario.Text;
+            string contraseña = txtPassword.Text;
+
+            if (usuario == "admin" && contraseña == "1234")
+            {
+                // Crear instancia del menú
+                FormMenu menu = new FormMenu();
+
+                // Mostrar menú
+                menu.Show();
+
+                // Ocultar login
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos");
+            }
         }
     }
 }
